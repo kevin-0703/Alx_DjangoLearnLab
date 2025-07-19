5ae0de5ff81498af964718b01fea505df484c03d
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-19 18:44:14 +0200

    managing user aunthentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -47,13 +47,13 @@ def register_view(request):
     return render(request, 'relationship_app/register.html', {'form': form})
 
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
 
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return hasattr(user, 'userprofile') and user.userprofile.role == "Librarian"
 
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return hasattr(user, 'userprofile') and user.userprofile.role == "Member"
 
 @login_required
 @user_passes_test(is_admin)
