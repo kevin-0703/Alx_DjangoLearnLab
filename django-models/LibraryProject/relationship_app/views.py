53527748f23a6abddbec1f55bc41938a514fb987
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-19 18:13:10 +0200

    managing user aunthentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,8 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
