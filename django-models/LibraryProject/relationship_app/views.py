3a801ac1d13b3865937b0d81bec91c3a0f6ee1df
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-19 15:42:43 +0200

    working with views and URL configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,9 +3,12 @@ from .models import Book, Library
 from django.views.generic import DetailView
 
 # Create your views here.
+
+
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library/library_detail.html'  # Adjust path to your templates folder
