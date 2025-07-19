e49740218b4b7df98ee3f831858c367f34a3b7c4
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-19 18:55:29 +0200

    managing user aunthentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # relationship_app/urls.py
 
 from django.urls import path
+from . import views
 from .views import list_books, LibraryDetailView
 from .views import login_view, logout_view, register_view, list_books, LibraryDetailView
 
