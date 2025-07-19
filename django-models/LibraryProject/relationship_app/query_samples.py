1691e8cc0a39f4fbc4c34c8b82ae86b47fc2ba99
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-19 12:50:01 +0200

    adding relatioship_app with models highlighting different types of relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,7 +7,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author=Author.objects.get(name=author_name)
-        books = Book.objects.filter( author=author)
+        books = Book.objects.filter(author=author)
         print(f"Books by {author_name}:")
         for book in books:
             print(f"- {book.title}")
