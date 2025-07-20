033b609c1e1a96b515b5f356be6344722877e740
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-20 10:38:33 +0200

    debbuging

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -61,7 +61,7 @@ def is_member(user):
 
 @login_required
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     return render(request, "relationship_app/admin_view.html")
 
 @login_required
