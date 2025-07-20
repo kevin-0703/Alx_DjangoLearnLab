070acf072f30de0f75713e18de86bd6aaee0fd7f
KEVIN NSHUTI <nkevin881@gmail.com>
2025-07-20 10:19:11 +0200

    debbuging

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
      path("register/", views.register_view, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-     path("admin-role/", views.admin_view, name="admin_view"),
+     path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-role/", views.librarian_view, name="librarian_view"),
     path("member-role/", views.member_view, name="member_view"),
      path("books/add/", views.add_book, name="add_book"),
