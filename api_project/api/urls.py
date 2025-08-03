dda3c35c99badd17e6eba29c076dfb30e7527727
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-03 13:17:02 +0200

    initial project setup

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookList
+from .views import BookList, BookViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookList)
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('books/', BookList.as_view({'get': 'list'}), name='book-list'),
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),  
+    path('', include('router.urls')),  
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,9 @@ from rest_framework import viewsets
 from rest_framework import generics
 
 # Create your views here.
-class BookList(generics.ListAPIView):
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+class BookViewSet(generics.RetriveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
