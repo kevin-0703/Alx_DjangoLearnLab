8182ab5705267bf1b88455d553a8f55166cad354
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-10 23:05:15 +0200

    intial project setup

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -12,11 +12,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(
-        Author,
-        related_name='books',  
-        on_delete=models.CASCADE
-    )
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} ({self.publication_year})"
--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -16,8 +16,8 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only=True)  
+    books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
-        fields = ['id', 'name', 'books']
+        fields = ['name', 'books']
