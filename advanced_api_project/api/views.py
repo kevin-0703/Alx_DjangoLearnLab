9d7c26be5dce388d752cf46e6b2bcbd3be377ff5
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-10 23:40:15 +0200

    adding filters

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
@@ -7,6 +8,12 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'publication_year', 'author']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year', 'author__name']
+    ordering = ['title']
+
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
