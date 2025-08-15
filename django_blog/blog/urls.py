2957ce14fb8dd01e73609224bbcd583fde9271c5
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-15 15:37:31 +0200

    creating authentication views and templates and configuring url paths

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("logout/", TemplateView.as_view(template_name="blog/logout.html"), name="logout"),
     path("login/", TemplateView.as_view(template_name="blog/login.html"), name="login"),
     path("register/", SignUpView.as_view(), name="templates/blog/signup"),
+    path("edit_profile/", TemplateView.as_view(template_name="blog/edit_profile.html"), name="edit_profile"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView 
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "blog/signup.html"
+
+@login_required
+def edit_profile(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        user = request.user
+        user.username = username
+        user.email = email
+        user.save()
+        messages.success(request, "Profile updated successfully.")
+        return redirect("edit_profile")
+    return render(request, "blog/edit_profile.html")
