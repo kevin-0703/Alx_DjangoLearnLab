54b6524beacefb028af27a73b5839794a624aeee
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-17 18:14:36 +0200

    creating tags

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path("comment/", CommentListView.as_view(), name="comment_list"),
     path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="add_comment"),
     path("", views.post_list, name="post_list"),
-    path("tags/<str:tag_name>/", views.posts_by_tag, name="posts_by_tag"),
     path("search/", views.search_posts, name="search_posts"),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 ]
