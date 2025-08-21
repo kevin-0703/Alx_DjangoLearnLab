f704f174ca3cfd95a513871dd643fc4640262165
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-21 18:51:28 +0200

    creating accounts app, implementing views and serializers and configuring urls

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -34,4 +34,5 @@ class RegisterSerializer(serializers.ModelSerializer):
                 email=validated_data['email'],
                 password=validated_data['password']
             )
+            Token.obkects.create(user=user)
             return user
