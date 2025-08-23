8341454c21c246fc966639e6ca6cbafb701e1774
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-23 13:42:02 +0200

    cadding following and unfollowing functionality to our and feed to our app

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -50,5 +50,5 @@ class FeedView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     def get_queryset(self):
-        followed_users = self.request.user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('following.all()') [permissions.IsAuthenticated]
