5884c668bc210ac09c502d55eb4aa086683bc613
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-23 16:54:14 +0200

    cimplementing liking and unliking functionality from our app

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework import generics, filters, viewsets
 from .models import Post, Comment
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from notifications.models import Notification
+from notifications.serializers import LikeNotificationSerializer, UnLikeNotificationDetailSerializer
+from posts.models import Like
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
@@ -44,3 +51,34 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
         if instance.author != self.request.user:
             raise PermissionError("You do not have permission to delete this comment.")
         instance.delete()
+
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        post = generics.get_object_or_404(Post, pk=pk)
+
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        if created:
+            # Create a notification for the post author
+            Notification.objects.create(
+                user=post.author,
+                actor=request.user,
+                verb="liked your post",
+                target=post
+            )
+            return Response({"message": "Post liked."}, status=status.HTTP_201_CREATED)
+        return Response({"message": "You already liked this post."}, status=status.HTTP_200_OK)
+
+
+class UnlikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
+        post = generics.get_object_or_404(Post, pk=pk)
+
+        like = Like.objects.filter(user=request.user, post=post).first()
+        if like:
+            like.delete()
+            return Response({"message": "Post unliked."}, status=status.HTTP_200_OK)
+        return Response({"message": "You haven’t liked this post yet."}, status=status.HTTP_400_BAD_REQUEST)
